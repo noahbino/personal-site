5bfe076d8e566cae2b5a465b36dabe3da9bc560e
noahbino <noahiarrobino@yahoo.com>
2024-08-05 23:06:46 -0400

    initial commit

---
+++ personal-website/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// src/firebaseConfig.js
+const firebaseConfig = {
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "personal-website-1ad43",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+  };
+  
+  export default firebaseConfig;
