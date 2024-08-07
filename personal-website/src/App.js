0f70289640602ee11936fda98f615dfa9a289efe
noahbino <noahiarrobino@yahoo.com>
2024-08-07 15:22:02 -0500

    update

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -6,13 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="link"
+          href="https://www.linkedin.com/in/noahiarrobino/"
           target="_blank"
           rel="noopener noreferrer"
         >
