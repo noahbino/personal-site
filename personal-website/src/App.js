8eaf1bf9770556f1872e7ccbed22bc0f2a2d61dd
noahbino <noahiarrobino@yahoo.com>
2024-12-18 18:53:44 -0500

    update

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -7,12 +7,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a
-          className="link"
+          className="App-link"
           href="https://www.linkedin.com/in/noahiarrobino/"
           target="_blank"
           rel="noopener noreferrer"
         >
           LinkedIn
+        </a>
+
+        <a
+          className="App-link"
+          href="https://github.com/noahbino"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
         </a>
       </header>
     </div>
