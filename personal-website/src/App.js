1c2ceecee1bcbbb37e3e675b65699703b9322385
noahbino <noahiarrobino@yahoo.com>
2024-12-30 01:03:01 -0500

    adding app store link

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';  // Import React here
+import React, { useEffect } from 'react';  // Import React here
 import './App.css';
-import { LinkedInLink, GitHubLink } from "./Components/Links";
+import { LinkedInLink, GitHubLink, AppStore } from "./Components/Links";
+import { analytics } from "./firebaseConfig"
+import { logEvent } from "firebase/analytics"
 
 function App() {
+useEffect(() => {
+  logEvent(analytics, "page_view", { page: "home"})
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="link-container">
           <LinkedInLink />
           <GitHubLink />
+          <AppStore />
         </div>
       </header>
     </div>
